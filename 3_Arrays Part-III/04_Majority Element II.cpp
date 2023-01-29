4de7ecef94790d03dccbe83960174a5fcbeeb383
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-01-30 00:33:10 +0530

    Create 04_Majority Element II.cpp

---
+++ 3_Arrays Part-III/04_Majority Element II.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int>ans;
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(auto it:nums){
+            mp[it]++;
+        }
+
+        for(auto it:mp){
+            if(it.second > n/3){
+                ans.push_back(it.first);
+            }
+        }
+
+        return ans;
+    }
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
